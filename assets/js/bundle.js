04de6729e35dbf3550c6bf76670b5d93386c2f50
Florian Margaine <florian@margaine.com>
2013-04-21 17:02:27 +0200

    Pretty menu

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -1,3 +1,6 @@
+/**
+ * Slides
+ */
 (function() {
     var slides = document.querySelectorAll('.slide');
     window.addEventListener('DOMContentLoaded', setSize);
@@ -9,5 +12,25 @@
         [].forEach.call(slides, function(slide) {
             slide.style.height = height + 'px';
         });
+    }
+}());
+
+/**
+ * Menu
+ */
+(function() {
+    var lis = document.querySelectorAll('#menu li'),
+        ul = document.querySelector('#menu ul');
+
+    window.addEventListener('DOMContentLoaded', setWidth);
+    window.addEventListener('resize', setWidth);
+
+    function setWidth() {
+        var width = 0;
+        [].forEach.call(lis, function(li) {
+            width += parseInt(window.getComputedStyle(li).width, 10);
+        });
+
+        ul.style.width = width + 'px';
     }
 }());
